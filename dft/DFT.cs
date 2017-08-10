f2ae19cc772bd9d8ca4bdd174e9a50e0693342bc
Hiroo Ishikawa <hiroo.ishikawa@gmail.com>
2017-08-10 18:30:19 +0900

    1st version of DFT implementation

--- dft/DFT.cs
+++ dft/DFT.cs
@@ -1,151 +1,63 @@
 using System;
 using System.IO;
 using System.Text;
-using System.Numerics;
-using System.Collections.Generic;
 
 public class DFT
 {
     private uint freq;
+    private const double PI2 = 2.0 * Math.PI;
 
     public DFT(uint frequency)
     {
         freq = frequency;
     }
 
-    static List<Complex> transform(List<UInt16> buffer)
+    public Tuple<double[], double[]> transform(short[] sample, int len)
     {
-        List<Complex> v = new List<Complex>(buffer.Count);
+        double[] re = new double[len];
+        double[] im = new double[len];
 
-        for (int i = 0; i < freq; i++) {
-            for (int j = 0; j < freq; j++) {
-                v[j] += new Complex(buffer[j], buffer[j]) * get_roter(j * partial);
+        for (int i = 0; i < len; i++) {
+            double tmp = PI2 * i / freq;
+            for (int j = 0; j < len; j++) {
+                re[i] += sample[j] * Math.Cos(tmp * j);
+                im[i] += sample[j] * Math.Sin(tmp * j);
             }
         }
 
-        return v;
+        return new Tuple<double[], double[]>(re, im);
     }
-
-    static Complex get_roter(int theta)
-    {
-        return new Complex(0, 0);
-    }
-
 }
 
-public class WaveReader
+class Test
 {
-    private uint fileSize;
-    private ushort nChannels;
-    private uint samplingRate;
-    private ushort depth;
-    private uint dataSize;
-    private BinaryReader reader;
-
-    public ushort NumChannels
-    {
-        get {
-            return nChannels;
-        }
-    }
-
-    public uint SamplingRate
-    {
-        get {
-            return samplingRate;
-        }
-    }
-
-    public ushort Depth
-    {
-        get {
-            return depth;
-        }
-    }
-
-    private void Load()
-    {
-        uint riffHeader = reader.ReadUInt32();
-        fileSize = reader.ReadUInt32();
-
-        uint riffType = reader.ReadUInt32();
-
-        uint chunkID = reader.ReadUInt32();
-        uint chunkSize = reader.ReadUInt32();
-
-        ushort formatId = reader.ReadUInt16();
-        nChannels = reader.ReadUInt16();
-        samplingRate = reader.ReadUInt32();
-        uint dataRate = reader.ReadUInt32();
-        ushort blockSize = reader.ReadUInt16();
-        depth = reader.ReadUInt16();
-
-        /*
-        if (dataRate != nChannels * blockSize) {
-        }
-        */
-
-        if (chunkSize > 16) {
-            ushort xSize = reader.ReadUInt16();
-            reader.ReadBytes(xSize);
-        }
-
-        uint dataID = reader.ReadUInt32();
-        dataSize = reader.ReadUInt32();
-    }
-
-    public WaveReader(Stream st) {
-        reader = new BinaryReader(st);
-        Load();
-    }
-
-    public void Close()
-    {
-        reader.Close();
-    }
-
-    public Int16 ReadInt16()
-    {
-        return reader.ReadInt16();
-    }
-
-    public UInt32 ReadUInt32()
-    {
-        return reader.ReadUInt32();
-    }
-
-    public Int16 ReadMonoral()
-    {
-        return reader.ReadInt16();
-    }
-
-    public Int16[] ReadStereo()
-    {
-        Int16[] data = new Int16[2];
-
-        data[0] = reader.ReadInt16();
-        data[1] = reader.ReadInt16();
-
-        return data;
-    }
-}
-
-class Test {
     static void Main(string[] args)
     {
         WaveReader reader = new WaveReader(File.OpenRead(args[0]));
+        DFT dft = new DFT(reader.SamplingRate);
+        StreamWriter writer = File.CreateText(args[1]);
+
         Console.WriteLine("Number of Channels: {0}", reader.NumChannels);
         Console.WriteLine("Sampling Rate: {0}", reader.SamplingRate);
         Console.WriteLine("Depth: {0}", reader.Depth);
 
         try {
-            for (;;) {
-                Console.WriteLine(reader.ReadInt16());
+            short[] sample = new short[reader.SamplingRate];
+            uint length = reader.SamplingRate / 2;
+
+            for (uint i = 0; i < reader.SamplingRate; i++) {
+                sample[i] = reader.ReadInt16();
+            }
+
+            Tuple<double[], double[]> result = dft.transform(sample, (int)length);
+            for (uint i = 0; i < length; i++) {
+                writer.WriteLine("{0} {1}", i, (int)result.Item1[i]);
             }
         }
         catch (System.IO.IOException e) {
         }
         finally {
+            writer.Close();
             reader.Close();
         }
     }
